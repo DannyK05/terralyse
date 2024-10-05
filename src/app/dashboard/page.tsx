cfb4b3b0b4be3fc0da9b56980c807cfe12e52dc5
DannyK05 <olubanjok@gmail.com>
2024-10-05 22:03:52 +0100

    starting code

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "terra-black": "var(--background)",
+        "terra-white": "var(--foreground)",
+        "terra-accent": "var(--light-brown)",
+        "terra-bg": "var(--dark-brown)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
