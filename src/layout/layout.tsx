b21ee0291a995b47ab871ae453d32aaf891a6773
DannyK05 <olubanjok@gmail.com>
2024-10-08 15:52:01 +0100

    chore: restyle the layout

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -7,27 +7,27 @@ export default function AppLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   const router = useRouter();
   return (
-    <div className="flex h-full items-center">
-      <nav className="bg-terra-accent flex fixed flex-col items-center justify-between h-full p-4">
-        <h1 onClick={() => router.push("/")} className="text-3xl text-white">
+    <div className="flex h-full items-start">
+      <nav className="bg-terra-accent w-1/5 flex fixed lg:flex-col items-center h-[100vh] p-4">
+        <h1 onClick={() => router.push("/")} className="text-6xl text-white">
           Terralyse
         </h1>
         <ul className="flex flex-col items-center space-y-10">
           <li
             onClick={() => router.push("/dashboard")}
-            className="p-4 cursor-pointer rounded-lg hover:bg-terra-accent text-2xl text-terra-white"
+            className="p-4 cursor-pointer rounded-lg hover:bg-terra-accent text-xl text-terra-white"
           >
             Dashboard
           </li>
           <li
             onClick={() => router.push("/map")}
-            className="p-4 cursor-pointer hover:bg-terra-accent text-terra-white text-2xl"
+            className="p-4 cursor-pointer hover:bg-terra-accent text-terra-white text-xl"
           >
             Map
           </li>
         </ul>
       </nav>
-      <main className="w-full">{children}</main>
+      <main className="w-3/4 lg:ml-[21rem] py-2">{children}</main>
     </div>
   );
 }
