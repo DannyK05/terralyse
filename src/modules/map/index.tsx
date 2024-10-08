df4496845bae655db5f1877f33729de0c7a57f77
DannyK05 <olubanjok@gmail.com>
2024-10-08 15:54:18 +0100

    fix: reduce the data feed to gemini for the prediction

--- src/modules/map/index.tsx
+++ src/modules/map/index.tsx
@@ -4,7 +4,12 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import soil_temp from "../../utilities/data/soil_temp.json";
 import soil_wetness from "../../utilities/data/soil_wetness.json";
 import wind_speed from "../../utilities/data/wind_speed.json";
+import humidity from "../../utilities/data/humidity.json";
 import AppLayout from "@/layout/layout";
+import { TSoilTemp } from "../dashboard/components/SoilTempGraph";
+import { TSoilWetness } from "../dashboard/components/SoilWetnessGraph";
+import { TWindSpeed } from "../dashboard/components/WindSpeedGraph";
+import { THumidity } from "../dashboard/components/HumidityGraph";
 export default function Map() {
   const mapKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
   const aiKey: string = process.env.NEXT_PUBLIC_GOOGLE_AI_API_KEY
@@ -15,10 +20,42 @@ export default function Map() {
     model: "gemini-1.5-flash",
   });
 
-  const prompt = `Using these summarized data, return a JSON of farming feasibility percentage based on these conditions: 
-Soil Temperature: ${JSON.stringify(soil_temp.slice(0, 10))},
-Soil Wetness: ${JSON.stringify(soil_wetness.slice(0, 10))}, 
-Wind Speed: ${JSON.stringify(wind_speed.slice(0, 10))}.`;
+  const soilTemp: TSoilTemp = [];
+  const soilWetness: TSoilWetness = [];
+  const windSpeed: TWindSpeed = [];
+  const Humidity: THumidity = [];
+
+  soil_temp.find((data) => {
+    if (data.YEAR === 2020 || data.YEAR === 2021) {
+      soilTemp.push(data);
+    }
+  });
+  soil_wetness.find((data) => {
+    if (
+      (data.PARAMETER === "rootSoil" && data.YEAR === 2020) ||
+      data.YEAR === 2021
+    ) {
+      soilWetness.push(data);
+    }
+  });
+  wind_speed.find((data) => {
+    if (data.YEAR === 2020 || data.YEAR === 2021) {
+      windSpeed.push(data);
+    }
+  });
+  humidity.find((data) => {
+    if (
+      (data.PARAMETER === "specificHumidity" && data.YEAR === 2020) ||
+      data.YEAR === 2021
+    ) {
+      Humidity.push(data);
+    }
+  });
+
+  const prompt = `Using these summarized data, return a JSON of farming feasibility, drought feasibility and flooding feasibility percentage based on these conditions: 
+Soil Temperature: ${JSON.stringify(soilTemp)},
+Soil Wetness: ${JSON.stringify(soilWetness)}, 
+Wind Speed: ${JSON.stringify(windSpeed)}.`;
   // const prompt = "List two types of flowers";
 
   const getResult = async () => {
@@ -32,7 +69,7 @@ Wind Speed: ${JSON.stringify(wind_speed.slice(0, 10))}.`;
   };
   const containerStyle = {
     width: "100%",
-    height: "80%",
+    height: "600px",
   };
 
   if (!mapKey) {
